8525b72837ca1431ffb6da2aa58f47f56006c200
Kayvon Taee <sma20kmt@sheffield.ac.uk>
2025-01-12 14:25:49 +0000

    Added a couple of tests

--- __tests__/filterData.ts
+++ __tests__/filterData.ts
@@ -1,30 +1,32 @@
 import {expect, it, describe} from '@jest/globals';
 import mock from "../__mocks__/mock";
-describe('FilterData', () => {
-    const names = [
-        "Employee Handbook",
-        "Public Holiday policy",
-        "Expenses",
-        "Expenses claim form",
-        "Fuel allowances",
-        "Cost centres",
-        "Misc",
-        "Christmas party",
-        "Welcome to the company!"
-    ]
-
+describe('filterData', () => {
     it("filters at the top level", () => {
         // Given a selection
         const selection = "Employee Handbook"
         // When we filter the data
-
+        const filteredData = filterData(mock, selection);
         // Then assert the filtered data is as expected
+        expect(filteredData.length).toBe(1);
+        expect(filteredData).toStrictEqual([{
+            "type": "pdf",
+            "name": "Employee Handbook",
+            "added": "2017-01-06",
+            "size": 1
+        }])
     })
     it("filters at the folder level", () => {
         // Given a selection
         const selection = "Fuel allowances"
         // When we filter the data
-
+        const filteredData = filterData(mock, selection);
         // Then assert the filtered data is as expected
+        expect(filteredData.length).toBe(1);
+        expect(filteredData).toStrictEqual([{
+            "type": "doc",
+            "name": "Fuel allowances",
+            "added": "2017-05-03",
+            "size": 4
+        }])
     })
 })
