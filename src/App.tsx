b60af31dcbf863a31183596944a395b9929381a0
Kayvon Taee <sma20kmt@sheffield.ac.uk>
2025-01-12 13:04:14 +0000

    Added a utility function

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import './App.css'
 import mock from "../__mocks__/mock_data.json"
 import {DocumentInterface} from "../utils/types/types.ts";
-import PairedDisplay from "./components/pairedDisplay.tsx";
-import {Typography} from "@mui/material";
+import renderFile from "./renderFiles.tsx";
+
 function App() {
     const data = mock as DocumentInterface
     return (
         <>
             {data.map(entry => {
-                if (entry.type === "folder" && "files" in entry) {
-                    entry.files.map(file => (
-                        <PairedDisplay label={file.name} key={file.name}>
-                            <>
-                                <Typography data-testid="paired-display-date">{file.added}</Typography>
-                                <Typography data-testid="paired-display-date">{file.size} KB</Typography>
-                            </>
-                        </PairedDisplay>
-                    ))
+                if ("files" in entry) {
+                    entry.files.map(file => renderFile(file));
+                } else {
+                    renderFile(entry)
                 }
             })}
         </>
