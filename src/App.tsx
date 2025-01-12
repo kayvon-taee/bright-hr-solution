aec4904909083a8d7f75ae26a44a37c0b633cff7
Kayvon Taee <sma20kmt@sheffield.ac.uk>
2025-01-12 13:16:37 +0000

    Folders were not being shown

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@
 import mock from "../__mocks__/mock_data.json"
 import {DocumentInterface} from "../utils/types/types.ts";
 import renderFile from "./renderFiles.tsx";
+import PairedDisplay from "./components/pairedDisplay.tsx";
 
 function App() {
     const data = mock as DocumentInterface
     return (
         <>
-            {data.map(entry => {
+            {data.map((entry) => {
                 if ("files" in entry) {
-                   return entry.files.map(file => renderFile(file));
+                    return (
+                        <PairedDisplay key={entry.name} label={entry.name}>
+                            <>
+                                {entry.files.map((file) => (
+                                    <PairedDisplay key={file.name} label={file.name}>
+                                        {renderFile(file)}
+                                    </PairedDisplay>
+                                ))}
+                            </>
+                        </PairedDisplay>
+                    );
                 } else {
-                   return renderFile(entry)
+                    return renderFile(entry);
                 }
             })}
         </>
-    )
+    );
 }
 
 export default App
