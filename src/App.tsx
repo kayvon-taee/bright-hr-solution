63cf359178d0e98acea2415bcec60cb1977a187e
Kayvon Taee <sma20kmt@sheffield.ac.uk>
2025-01-12 13:22:34 +0000

    Added accordion

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import mock from "../__mocks__/mock_data.json"
 import {DocumentInterface} from "../utils/types/types.ts";
 import renderFile from "./renderFiles.tsx";
 import PairedDisplay from "./components/pairedDisplay.tsx";
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
     const data = mock as DocumentInterface
@@ -11,15 +13,25 @@ function App() {
             {data.map((entry) => {
                 if ("files" in entry) {
                     return (
-                        <PairedDisplay key={entry.name} label={entry.name}>
-                            <>
-                                {entry.files.map((file) => (
-                                    <PairedDisplay key={file.name} label={file.name}>
-                                        {renderFile(file)}
-                                    </PairedDisplay>
-                                ))}
-                            </>
-                        </PairedDisplay>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                            >
+                                <Typography component="span">{entry.name}</Typography>
+                            </AccordionSummary>
+                        <AccordionDetails>
+                            {entry.files.map((file) => (
+                                <PairedDisplay key={file.name} label={file.name}>
+                                    <>
+                                        <Typography data-testid="paired-display-date">{file.added}</Typography>
+                                        <Typography data-testid="paired-display-date">{file.size} KB</Typography>
+                                    </>
+                                </PairedDisplay>
+                            ))}
+                        </AccordionDetails>
+                        </Accordion>
                     );
                 } else {
                     return renderFile(entry);
